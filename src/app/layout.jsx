1c8059a8957aaf114d26849d4f988ebe9a238e0c
sunnyheee <fool0116@naver.com>
2024-05-19 14:19:04 +0900

    create project

---
+++ src/app/layout.jsx
@@ -0,0 +1,20 @@
+import { Noto_Sans_KR } from 'next/font/google'
+import './globals.css'
+
+const notoSansKr = Noto_Sans_KR({
+  weight: ['400'],
+  subsets: ['latin'],
+})
+
+export const metadata = {
+  title: 'DessertTime',
+  description: '',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body className={notoSansKr.className}>{children}</body>
+    </html>
+  )
+}
