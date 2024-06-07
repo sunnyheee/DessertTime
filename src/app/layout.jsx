ff493a037dfad73471c49090423a035656e98e97
sunnyheee <fool0116@naver.com>
2024-06-07 22:33:45 +0900

    chore: setting width

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body className={notoSansKr.className}>{children}</body>
+      <body className={`wrapper ${notoSansKr.className}`}>{children}</body>
     </html>
   )
 }
