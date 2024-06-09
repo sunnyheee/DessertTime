758af09e9042044e5eee4f12dd87bab9ba0ec9b1
[qkrrkddud] <[ejfehd1@naver.com]>
2024-06-09 14:40:23 +0900

    test

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,28 +1,26 @@
-import { Noto_Sans_KR } from 'next/font/google'
-import './globals.css'
-import './reset.css'
-import Footer from './components/common/Footer'
+"use client"; 
+import { Noto_Sans_KR } from 'next/font/google';
+import './globals.css';
+import './reset.css';
+import Footer from './components/common/Footer';
 
 const notoSansKr = Noto_Sans_KR({
   weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
 });
 
-export const metadata = {
-  title: 'DessertTime',
-  description: '',
-};
+// export const metadata = {
+//   title: 'DessertTime',
+//   description: '',
+// };
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
-  const noFooterPages = ['/login']; // Add any other paths where footer should be hidden
-
-  const showFooter = !noFooterPages.includes(pathname);
 
   return (
     <html lang="ko">
-      <body className={`wrapper ${notoSansKr.className}`}>{children}
-        
+      <body className={`wrapper ${notoSansKr.className}`}>
+        {children}
+       <Footer />
       </body>
     </html>
   );
