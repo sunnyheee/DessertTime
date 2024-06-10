53e5dd7be9f1aa7eecb7913be6c97a1cbdd011ec
sunnyheee <fool0116@naver.com>
2024-06-10 21:49:37 +0900

    edit: footer main css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,43 +1,27 @@
-"use client"
-import { useEffect, useState } from 'react';
-import dynamic from 'next/dynamic'; 
-import { Noto_Sans_KR } from 'next/font/google';
-import './globals.css';
-import './reset.css';
+import { Noto_Sans_KR } from 'next/font/google'
 
-const Footer = dynamic(() => import('./components/common/Footer'), { ssr: false }); 
+import './globals.css'
+import './reset.css'
+import Footer from './components/common/Footer'
+
 const notoSansKr = Noto_Sans_KR({
   weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
-});
+})
 
-// use client 쓰려면 이게 자꾸 오류가떠서 일단 주석처리해놨습니다..
-// export const metadata = {
-//   title: 'DessertTime',
-//   description: '',
-// };
+export const metadata = {
+  title: 'DessertTime',
+  description: '',
+}
 
 export default function RootLayout({ children }) {
-  const [hideFooter, setHideFooter] = useState(false);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const pathname = window.location.pathname;
-      if (pathname.startsWith('/login')) {
-        setHideFooter(true);
-      
-      } else {
-        setHideFooter(false);
-      }
-    }
-  }, []);
-
   return (
     <html lang="ko">
-      <body className={`wrapper ${notoSansKr.className}`}>
-        {children}
-        {!hideFooter && <Footer />}
+      <body className={notoSansKr.className}>
+        <main className="main">
+          <div className="wrapper">{children}</div>
+        </main>
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,13 @@
 'use client'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
-import { Pagination, Autoplay } from 'swiper/modules';
-import Image from 'next/image';
-import styles from './page.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+import { Pagination, Autoplay } from 'swiper/modules'
+import Image from 'next/image'
+import styles from './page.module.css'
+import Footer from './components/common/Footer'
 
 export default function Home() {
-  const slides = ['/images/image1.png', '/images/image2.png'];
+  const slides = ['/images/image1.png', '/images/image2.png']
 
   const reviews = [
     {
@@ -45,56 +46,83 @@ export default function Home() {
         '/images/image10.png',
       ],
     },
-  ];
+  ]
 
-
-  
   return (
-    <main className={styles.main}>
-      <div className={styles.header}>
-        <Image src="/images/dessertTime.png" alt="메인로고" width={216} height={29}   style={{ cursor: 'pointer' }}/>
-        <div className={styles.icon}>
-          <Image src="/images/search.png" alt="메인서치" width={18} height={18} />
-          <Image src="/images/bell.png" alt="메인알림" width={24} height={24} />
-        </div>
-      </div>
-      <Swiper
-        spaceBetween={8}
-        slidesPerView="auto"
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        loop={true}
-        pagination={{ clickable: true }}
-        modules={[Pagination, Autoplay]}
-        className={styles.swiperContainer}
-      >
-        {slides.map((slide, index) => (
-          <SwiperSlide key={index} className={styles.swiperSlide}>
-            <Image src={slide} alt={`Slide ${index + 1}`} width={348} height={214} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      {reviews.map((review, index) => (
-        <div key={index} className={styles.reviewSection}>
-          <h2 className={styles.reviewtxt}>{review.name}</h2>
-          <div className={styles.swiperContainer2}>
-            <Swiper
-              spaceBetween={8}
-              slidesPerView="auto"
-              loop={true}
-              className={styles.slidewrap}
-            >
-              {review.images.map((image, imageIndex) => (
-                <SwiperSlide key={imageIndex} className={styles.swiperSlide2}>
-                  <Image src={image} alt={`Review Image ${index + 1}`} width={106} height={140} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
+    <>
+      <section className={styles.homeSec}>
+        <div className={styles.header}>
+          <Image
+            src="/images/dessertTime.png"
+            alt="메인로고"
+            width={216}
+            height={29}
+            style={{ cursor: 'pointer' }}
+          />
+          <div className={styles.icon}>
+            <Image
+              src="/images/search.png"
+              alt="메인서치"
+              width={18}
+              height={18}
+            />
+            <Image
+              src="/images/bell.png"
+              alt="메인알림"
+              width={24}
+              height={24}
+            />
           </div>
         </div>
-      ))}
-    </main>
-  );
+        <Swiper
+          spaceBetween={8}
+          slidesPerView="auto"
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          pagination={{ clickable: true }}
+          modules={[Pagination, Autoplay]}
+          className={styles.swiperContainer}
+        >
+          {slides.map((slide, index) => (
+            <SwiperSlide key={index} className={styles.swiperSlide}>
+              <Image
+                src={slide}
+                alt={`Slide ${index + 1}`}
+                width={348}
+                height={214}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        {reviews.map((review, index) => (
+          <div key={index} className={styles.reviewSection}>
+            <h2 className={styles.reviewtxt}>{review.name}</h2>
+            <div className={styles.swiperContainer2}>
+              <Swiper
+                spaceBetween={8}
+                slidesPerView="auto"
+                loop={true}
+                className={styles.slidewrap}
+              >
+                {review.images.map((image, imageIndex) => (
+                  <SwiperSlide key={imageIndex} className={styles.swiperSlide2}>
+                    <Image
+                      src={image}
+                      alt={`Review Image ${index + 1}`}
+                      width={106}
+                      height={140}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
+        ))}
+      </section>
+      <Footer />
+    </>
+  )
 }
