5330e3248376a31b76f06366dedf59e852d586ba
[qkrrkddud] <[ejfehd1@naver.com]>
2024-06-12 02:37:38 +0900

    공통헤더작업

---
+++ src/app/components/common/Header.jsx
@@ -0,0 +1,57 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import styles from './Header.module.css'
+
+const Header = ({
+  title,
+  showBackButton = true,
+  showIcons = true,
+  showMainLogo = true,
+}) => {
+  const router = useRouter()
+
+  const prev = () => {
+    router.back()
+  }
+ 
+  const scrollToTop = () => {
+    window.scrollTo(0, 0); 
+  }
+
+  return (
+    <div className={styles.head}>
+      {showBackButton && (
+        <Image
+          src="/images/icon.png"
+          alt="이전"
+          width={14}
+          height={14}
+          onClick={prev}
+          className={styles.backButton}
+        />
+      )}
+      {showMainLogo && (
+        <Image
+          src="/images/dessertTime.png"
+          alt="메인로고"
+          width={216}
+          height={29}
+          className={styles.mainLogo}
+          style={{ cursor: 'pointer' }}
+          onClick={scrollToTop}
+        />
+      )}
+      <h2 className={styles.title}>{title}</h2>
+      {showIcons && (
+        <div className={styles.icon}>
+          <Image src="/images/search.png" alt="검색" width={18} height={18} />
+          <Image src="/images/bell.png" alt="알림" width={24} height={24} />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Header
--- src/app/login/inquiry/page.jsx
+++ src/app/login/inquiry/page.jsx
@@ -1,17 +1,15 @@
 "use client";
 import React, { useState } from 'react';
-import Image from 'next/image';
 import styles from './inquiry.module.css';
+import Header from '../../components/common/Header';
 import { useRouter } from 'next/navigation';
+
 export default function Inquiry() {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const [isButtonEnabled, setIsButtonEnabled] = useState(false);
     const router = useRouter();
 
-    const prev = ()=>{
-        router.push('/login');
-    }
     const handleEmailChange = (e) => {
         const { value } = e.target;
         setEmail(value);
@@ -33,42 +31,36 @@ export default function Inquiry() {
     };
 
     const handleSubmit = () => {
-        router.push('/login/success')
+        router.push('/login/success');
     };
 
     return (
         <main className={styles.main}>
             <div className={styles.container}>
-                <div className={styles.head}>
-                    <Image src='/images/icon.png' alt="이전" width={14} height={14} 
-                    onClick={prev}/>
-                    <h2 className={styles.title}>
-                        문의하기
-                    </h2>
-                </div>
+                <Header title="문의하기" showBackButton={true} showIcons={false}/>
                 <div className={styles.content}>
-                <p className={styles.email}>답변용 이메일</p>
-                <input
-                    type="email"
-                    className={styles.input}
-                    placeholder="답변받으실 이메일을 입력하세요"
-                    value={email}
-                    onChange={handleEmailChange}
-                />
-                <p className={styles.con}>문의내용</p>
-                <textarea
-                    className={styles.textarea}
-                    placeholder="최대 300자로 입력 가능합니다"
-                    value={message}
-                    onChange={handleMessageChange}
-                ></textarea>
-                <button
-                    className={`${styles.btn} ${isButtonEnabled ? styles.btnActive : ''}`}
-                    onClick={handleSubmit}
-                    disabled={!isButtonEnabled}
-                >
-                    보내기
-                </button>
+                    <p className={styles.email}>답변용 이메일</p>
+                    <input
+                        type="email"
+                        className={styles.input}
+                        placeholder="답변받으실 이메일을 입력하세요"
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                    <p className={styles.con}>문의내용</p>
+                    <textarea
+                        className={styles.textarea}
+                        placeholder="최대 300자로 입력 가능합니다"
+                        value={message}
+                        onChange={handleMessageChange}
+                    ></textarea>
+                    <button
+                        className={`${styles.btn} ${isButtonEnabled ? styles.btnActive : ''}`}
+                        onClick={handleSubmit}
+                        disabled={!isButtonEnabled}
+                    >
+                        보내기
+                    </button>
                 </div>
             </div>
         </main>
