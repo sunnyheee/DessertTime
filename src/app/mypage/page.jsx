1afd295e5cd5e9dacb8f7f42e7192622a2ebc23f
sunnyheee <fool0116@naver.com>
2024-07-03 22:48:49 +0900

    edit: 내 후기 디테일 링크 설치

--- src/app/mypage/page.jsx
+++ src/app/mypage/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Footer from '../_components/common/Footer'
 import Image from 'next/image'
+import Link from 'next/link'
+import Footer from '../_components/common/Footer'
 import styles from './page.module.css'
 
 const Mypage = () => {
@@ -9,14 +10,14 @@ const Mypage = () => {
       <section className="sec">
         <div className={styles.wrap}>
           <div className={styles.head}>
-            <a href='/mypage/setting'>
-            <Image
-              src="/images/setting.png"
-              alt="setting"
-              width={20}
-              height={20}
-              className={styles.setting}
-            />
+            <a href="/mypage/setting">
+              <Image
+                src="/images/setting.png"
+                alt="setting"
+                width={20}
+                height={20}
+                className={styles.setting}
+              />
             </a>
           </div>
           <div className={styles.content}>
@@ -30,66 +31,62 @@ const Mypage = () => {
             <h1 className={styles.nickname}>1234567번째 달달한 고구마</h1>
             <button className={styles.correction}>내 정보 수정</button>
             <div className={styles.firstWrap}>
-              <div className={styles.myReview}>
-                <p className={styles.left}>내 후기</p>
-                <div className={styles.right}>
-                  <p className={styles.txt}>23 개</p>
-                  <a href='/review/myReview'>
-                  <Image
-                  src="/images/chevronRight.png"
-                  alt='chevronRight'
-                  width={7.4}
-                  height={12}
-                  className={styles.chevronRight}
-                  />
-                  </a>
+              <Link href="/review/myReview">
+                <div className={styles.myReview}>
+                  <p className={styles.left}>내 후기</p>
+                  <div className={styles.right}>
+                    <p className={styles.txt}>23 개</p>
+
+                    <Image
+                      src="/images/chevronRight.png"
+                      alt="chevronRight"
+                      width={7.4}
+                      height={12}
+                      className={styles.chevronRight}
+                    />
+                  </div>
                 </div>
-              </div>
+              </Link>
               <div className={styles.myReview}>
                 <p className={styles.left}>보유 밀</p>
-                <div className={styles.right}>
-                  <div className={styles.txtWrap}>
-                  <p className={styles.txt}>3,000</p>
-                  <a href=''>
-                  <Image
-                    src="/images/meal.png"
-                    width={18}
-                    height={18}
-                    alt='meal'
-                    className={styles.meal}
-                  />
-                  </a>
+                <Link href="">
+                  <div className={styles.right}>
+                    <div className={styles.txtWrap}>
+                      <p className={styles.txt}>3,000</p>
+
+                      <Image
+                        src="/images/meal.png"
+                        width={18}
+                        height={18}
+                        alt="meal"
+                        className={styles.meal}
+                      />
+                    </div>
+                    <Image
+                      src="/images/chevronRight.png"
+                      alt="chevronRight"
+                      width={7.4}
+                      height={12}
+                      className={styles.chevronRight}
+                    />
                   </div>
-                  <Image
-                  src="/images/chevronRight.png"
-                  alt='chevronRight'
-                  width={7.4}
-                  height={12}
-                  className={styles.chevronRight}
-                  />
-                </div>
+                </Link>
               </div>
             </div>
             <div className={styles.twiceWrap}>
-              <a href='' className={styles.box}>
+              <a href="" className={styles.box}>
                 <img
                   src="/images/announcement.png"
                   className={styles.boxImage}
                 />
                 <p className={styles.boxTxt}>공지사항</p>
               </a>
-              <a href='' className={styles.box}>
-                <img
-                  src="/images/gift.png"
-                  className={styles.boxImage}
-                />
+              <a href="" className={styles.box}>
+                <img src="/images/gift.png" className={styles.boxImage} />
                 <p className={styles.boxTxt}>이벤트</p>
               </a>
-              <a href='/inquiry' className={styles.box}>
-                <img
-                  src="/images/inquiry.png"
-                  className={styles.boxImage}
-                />
+              <a href="/inquiry" className={styles.box}>
+                <img src="/images/inquiry.png" className={styles.boxImage} />
                 <p className={styles.boxTxt}>문의사항</p>
               </a>
             </div>
--- src/app/review/myReview/page.jsx
+++ src/app/review/myReview/page.jsx
@@ -1,40 +1,41 @@
 'use client'
-import styles from './page.module.css'
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from './page.module.css'
 import Header from '../../_components/common/Header'
 import Footer from '../../_components/common/Footer'
-import Image from 'next/image'
 import StarIcon from '../../_components/icon/StarIcon'
-import { useRouter } from 'next/navigation'
 
 const MyReview = () => {
   const reviews = [
     {
       id: 1,
-      store: "온혜화",
-      dessert: "바질치즈스콘",
+      store: '온혜화',
+      dessert: '바질치즈스콘',
       stars: 3,
-      imagePath: "/images/scon.png",
+      imagePath: '/images/scon.png',
       state: 1,
       year: 2024,
       month: 6,
     },
     {
       id: 2,
-      store: "또 다른 매장",
-      dessert: "초콜릿 케이크",
+      store: '또 다른 매장',
+      dessert: '초콜릿 케이크',
       stars: 4,
-      imagePath: "/images/scon.png",
+      imagePath: '/images/scon.png',
       state: 0,
       year: 2024,
       month: 6,
     },
     {
       id: 3,
-      store: "또 다른 매장2",
-      dessert: "딸기 케이크",
+      store: '또 다른 매장2',
+      dessert: '딸기 케이크',
       stars: 2,
-      imagePath: "/images/scon.png",
+      imagePath: '/images/scon.png',
       state: 3,
       year: 2024,
       month: 5,
@@ -42,7 +43,7 @@ const MyReview = () => {
   ]
 
   const router = useRouter()
-  
+
   const [showModal, setShowModal] = useState(false)
   const [activeReviewId, setActiveReviewId] = useState(null)
   const [deleteReviewId, setDeleteReviewId] = useState(null)
@@ -57,7 +58,7 @@ const MyReview = () => {
 
   const handleDotsClick = (id, event) => {
     event.stopPropagation()
-    setActiveReviewId(prevState => prevState === id ? null : id)
+    setActiveReviewId((prevState) => (prevState === id ? null : id))
   }
 
   const handleDeleteClick = (reviewId) => {
@@ -70,11 +71,31 @@ const MyReview = () => {
   }
 
   const getStatusText = (state) => {
-    switch(state) {
-      case 1: return { text: '등록', color: '#048c44', backgroundColor: 'rgba(20, 174, 92, 0.2)' }
-      case 2: return { text: '대기', color: '#068dff', backgroundColor: 'rgba(6, 141, 255, 0.2)' }
-      case 3: return { text: '신고', color: '#ef4444', backgroundColor: 'rgba(239, 68, 68, 0.2)' }
-      default: return { text: '삭제', color: '#888888', backgroundColor: 'rgba(136, 136, 136, 0.2)' }
+    switch (state) {
+      case 1:
+        return {
+          text: '등록',
+          color: '#048c44',
+          backgroundColor: 'rgba(20, 174, 92, 0.2)',
+        }
+      case 2:
+        return {
+          text: '대기',
+          color: '#068dff',
+          backgroundColor: 'rgba(6, 141, 255, 0.2)',
+        }
+      case 3:
+        return {
+          text: '신고',
+          color: '#ef4444',
+          backgroundColor: 'rgba(239, 68, 68, 0.2)',
+        }
+      default:
+        return {
+          text: '삭제',
+          color: '#888888',
+          backgroundColor: 'rgba(136, 136, 136, 0.2)',
+        }
     }
   }
 
@@ -88,135 +109,141 @@ const MyReview = () => {
   }, {})
 
   return (
-    <>
-      <section className="sec">
-        <div className={styles.wrap}>
-          <Header
-            title="내 후기"
-            showBackButton={true}
-            showIcons={true}
-            showMainLogo={false}
-          />
-          {Object.keys(groupedReviews).map((key) => {
-            const [year, month] = key.split('-')
-            return (
-              <div key={key} className={styles.content}>
-                <p className={styles.date}>{`${year}년 ${month}월`}</p>
-                {groupedReviews[key].map((review) => {
-                  const status = getStatusText(review.state)
-                  const stateStyle = {
-                    color: status.color,
-                    backgroundColor: status.backgroundColor,
-                  }
-                  return (
-                    <div key={review.id} className={styles.container} onClick={handleContainerClick}>
-                      {review.state === 0 && (
-                        <div className={styles.overlay}>
-                          관리자에 의해 삭제된 후기입니다.
-                        </div>
-                      )}
-                      {review.state === 3 && (
-                        <div className={styles.overlay}>
-                          신고된 후기입니다.
-                        </div>
-                      )}
-                      <Image
-                        className={styles.image}
-                        src={review.imagePath}
-                        width={108}
-                        height={80}
-                        alt="dessert"
-                      />
-                      <div className={styles.txtBox}>
-                        <div className={styles.left}>
-                          <p className={styles.store}>{review.store}</p>
-                          <h2 className={styles.dessert}>{review.dessert}</h2>
-                          {Array.from({ length: 4 }).map((_, i) => (
-                            <StarIcon
-                              key={i}
-                              fill={i < review.stars ? '#FFA629' : '#CCCCCC'}
-                              className={styles.icon}
-                              width={20}
-                              height={20}
-                            />
-                          ))}
-                        </div>
-                        <div className={styles.right}>
-                          <p className={styles.state} style={stateStyle}>
-                            {status.text}
-                          </p>
+    <section className="sec">
+      <div className={styles.wrap}>
+        <Header
+          title="내 후기"
+          showBackButton={true}
+          showIcons={true}
+          showMainLogo={false}
+        />
+        {Object.keys(groupedReviews).map((key) => {
+          const [year, month] = key.split('-')
+          return (
+            <div key={key} className={styles.content}>
+              ccc
+              <p className={styles.date}>{`${year}년 ${month}월`}</p>
+              {groupedReviews[key].map((review) => {
+                const status = getStatusText(review.state)
+                const stateStyle = {
+                  color: status.color,
+                  backgroundColor: status.backgroundColor,
+                }
+                return (
+                  <Link
+                    href={`/review/detail/${review.id}`}
+                    key={review.id}
+                    className={styles.container}
+                    onClick={handleContainerClick}
+                  >
+                    {review.state === 0 && (
+                      <div className={styles.overlay}>
+                        관리자에 의해 삭제된 후기입니다.
+                      </div>
+                    )}
+                    {review.state === 3 && (
+                      <div className={styles.overlay}>신고된 후기입니다.</div>
+                    )}
+                    <Image
+                      className={styles.image}
+                      src={review.imagePath}
+                      width={108}
+                      height={80}
+                      alt="dessert"
+                    />
+                    <div className={styles.txtBox}>
+                      <div className={styles.left}>
+                        <p className={styles.store}>{review.store}</p>
+                        <h2 className={styles.dessert}>{review.dessert}</h2>
+                        {Array.from({ length: 4 }).map((_, i) => (
+                          <StarIcon
+                            key={i}
+                            fill={i < review.stars ? '#FFA629' : '#CCCCCC'}
+                            className={styles.icon}
+                            width={20}
+                            height={20}
+                          />
+                        ))}
+                      </div>
+                      <div className={styles.right}>
+                        <p className={styles.state} style={stateStyle}>
+                          {status.text}
+                        </p>
+                        <button
+                          className={styles.dotsButton}
+                          onClick={(event) => handleDotsClick(review.id, event)}
+                          type="button"
+                        >
                           <div
-                            className={styles.dotsButton}
-                            onClick={(event) => handleDotsClick(review.id, event)}
-                            type="button"
-                          >
-                            <div
-                              className={`${styles.dot} ${activeReviewId === review.id ? styles.active : ''}`}
-                            ></div>
-                            <div
-                              className={`${styles.dot} ${activeReviewId === review.id ? styles.active : ''}`}
-                            ></div>
-                            <div
-                              className={`${styles.dot} ${activeReviewId === review.id ? styles.active : ''}`}
-                            ></div>
-                            <div className={styles.deletewrap}>
-                              {activeReviewId === review.id && (
-                                <di
-                                  className={`${styles.deleteContainer} ${styles.showDeleteContainer}`}
-                                  onClick={() => handleDeleteClick(review.id)}
+                            className={`${styles.dot} ${activeReviewId === review.id ? styles.active : ''}`}
+                          ></div>
+                          <div
+                            className={`${styles.dot} ${activeReviewId === review.id ? styles.active : ''}`}
+                          ></div>
+                          <div
+                            className={`${styles.dot} ${activeReviewId === review.id ? styles.active : ''}`}
+                          ></div>
+                          <div className={styles.deletewrap}>
+                            {activeReviewId === review.id && (
+                              <di
+                                className={`${styles.deleteContainer} ${styles.showDeleteContainer}`}
+                                onClick={() => handleDeleteClick(review.id)}
+                                type="button"
+                              >
+                                <Image
+                                  src="/images/delete.png"
+                                  width={18}
+                                  height={18}
+                                  alt="쓰레기통"
+                                />
+                                <button
+                                  className={styles.deleteButton}
                                   type="button"
                                 >
-                                  <Image
-                                    src="/images/delete.png"
-                                    width={18}
-                                    height={18}
-                                    alt="쓰레기통"
-                                  />
-                                  <button className={styles.deleteButton} type="button">
-                                    삭제하기
-                                  </button>
-                                </di>
-                              )}
-                            </div>
+                                  삭제하기
+                                </button>
+                              </di>
+                            )}
                           </div>
-                        </div>
+                        </button>
                       </div>
                     </div>
-                  )
-                })}
-              </div>
-            )
-          })}
-        </div>
-        <Footer activeButton="mypage" />
-        {showModal && (
-          <div className={styles.modal}>
-            <div className={styles.modalContent}>
-              <p className={styles.check}>
-                삭제한 후기는 복구가 불가능합니다.<br/>
-                정말 삭제 하시겠습니까?
-              </p>
-              <div className={styles.modalButtons}>
-                <button
-                  className={`${styles.modalButton} ${styles.confirmButton}`}
-                  onClick={handleConfirmDelete}
-                  type="button"
-                >
-                  삭제하기
-                </button>
-                <button
-                  className={`${styles.modalButton} ${styles.cancelButton}`}
-                  onClick={handleCancelDelete}
-                  type="button"
-                >
-                  아니오
-                </button>
-              </div>
+                  </Link>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+      <Footer activeButton="mypage" />
+      {showModal && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <p className={styles.check}>
+              삭제한 후기는 복구가 불가능합니다.
+              <br />
+              정말 삭제 하시겠습니까?
+            </p>
+            <div className={styles.modalButtons}>
+              <button
+                className={`${styles.modalButton} ${styles.confirmButton}`}
+                onClick={handleConfirmDelete}
+                type="button"
+              >
+                삭제하기
+              </button>
+              <button
+                className={`${styles.modalButton} ${styles.cancelButton}`}
+                onClick={handleCancelDelete}
+                type="button"
+              >
+                아니오
+              </button>
             </div>
           </div>
-        )}
-      </section>
-    </>
+        </div>
+      )}
+    </section>
   )
 }
 
